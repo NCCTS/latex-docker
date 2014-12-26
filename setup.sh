8c22388f94058a7ad7b519e1fe7d5cd15ff59d5b
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-26 02:10:44 -0600

    revise comment

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export HOME=/root
 apt-get update
 apt-get -y install autoconf automake dpkg-dev g++ gcc imagemagick libc6-dev libdb5.3-dev libgdbm-dev libxslt1-dev make perl-doc perlmagick poppler-utils software-properties-common zlib1g-dev
 
-# Setup LaTeX environment as unprivileged user 'sailor'
+# Download, setup LaTeX environment as unprivileged user 'sailor'
 mkdir -p /usr/local/texlive
 chmod -R 777 /usr/local/texlive/
 sudo -i -u sailor /docker-build/support/user_sailor.sh
