7a0ad6fcb56bb4b1ebb11d30e8df9324448f436c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-26 02:09:33 -0600

    do a network-based install per support/repository.txt

--- support/user_sailor.sh
+++ support/user_sailor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Assumes 'mirror.sh' has been run to completion on the docker host
-cd /docker-build/texlive-mirror
+cd /docker-build/support
+repo=$(cat repository.txt)
+wget $repo/install-tl-unx.tar.gz
+tar xvzf install-tl-unx.tar.gz
+cd $(dirname -- $(ls install-tl-*/install-tl))
+
 ./install-tl -profile /docker-build/support/texlive-installation.profile \
-             -repository /docker-build/texlive-mirror
+             -repository $repo
 
 cat /docker-build/support/bashrc_append_sailor.txt >> $HOME/.bashrc
