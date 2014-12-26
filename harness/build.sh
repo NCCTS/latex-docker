736b7943ad2bc1d2261c335766a0423e848b6ba9
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-26 02:12:50 -0600

    build-harness scripts

---
+++ harness/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+script_path () {
+    local scr_path=""
+    local dir_path=""
+    local sym_path=""
+    # get (in a portable manner) the absolute path of the current script
+    scr_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && scr_path=$scr_path/$(basename -- "$0")
+    # if the path is a symlink resolve it recursively
+    while [ -h $scr_path ]; do
+        # 1) cd to directory of the symlink
+        # 2) cd to the directory of where the symlink points
+        # 3) get the pwd
+        # 4) append the basename
+        dir_path=$(dirname -- "$scr_path")
+        sym_path=$(readlink $scr_path)
+        scr_path=$(cd $dir_path && cd $(dirname -- "$sym_path") && pwd)/$(basename -- "$sym_path")
+    done
+    echo $scr_path
+}
+
+script_dir=$(dirname -- "$(script_path)")
+cd $script_dir/..
+
+quad_rand () {
+    echo $RANDOM"-"$RANDOM"-"$RANDOM"-"$RANDOM
+}
+
+# Assumes boot2docker on a Mac with default '/Users/...' mount config
+data_name=data-$(quad_rand)
+docker run \
+       -it \
+       --name $data_name \
+       -v $(pwd):/home/sailor/latex-docker \
+       nccts/baseimage \
+       /bin/true
+
+mirr_name=mirr-$(quad_rand)
+docker run \
+       -d \
+       --name $mirr_name \
+       nccts/texlive-mirror:2014
+
+builder_name=builder-$(quad_rand)
+docker run \
+       --privileged \
+       -it --rm \
+       --name $builder_name \
+       --link $mirr_name:mirr \
+       -v /var/run:/var/docker_host/run \
+       --volumes-from $data_name \
+       --entrypoint /sbin/my_init \
+       nccts/builder \
+       -- bash -i -c /home/sailor/latex-docker/harness/dind.sh
+
+docker kill $mirr_name
+docker rm $mirr_name
+docker rm $data_name
---
+++ harness/dind.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mirr=($(echo $(echo $MIRR_PORT | tr 'tcp://' '\n')))
+mirr_host=${mirr[0]}
+mirr_port=${mirr[1]}
+mirr_http=http://$mirr_host:$mirr_port/
+
+# Wait for the mirror's port to open
+while true; do
+    nc $mirr_host $mirr_port < /dev/null
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
+done
+
+cd /home/sailor
+mkdir temp
+cd temp
+git clone ../latex-docker
+cd latex-docker
+echo $mirr_http > support/repository.txt
